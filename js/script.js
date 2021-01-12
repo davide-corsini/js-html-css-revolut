d42ec9e8f83c89574b6c62af1ce4cd632b683012
davide <davidecorsinimilano@gmail.com>
2021-01-12 17:09:53 +0100

    Exercise + bonus end

--- js/script.js
+++ js/script.js
@@ -3,8 +3,11 @@ $(document).ready(function(){
     $('.with-dropdown').hover(function(){
         $(this).find('.dropdown-menu').toggleClass('active');
     });
-
+    
     $('.with-dropdown-click').click(function(){
         $(this).find('.dropdown-menu.lenguage').toggleClass('active');
+        $(this).find('i#arrow-down').toggleClass("fa-arrow-down fa-arrow-up")
     });
 });
+
+/* <i id="arrow-up" class="fas fa-arrow-up"></i> */
