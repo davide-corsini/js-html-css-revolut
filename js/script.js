e23f5af2eca435aa1f0aabf936b2a9853fa26e3c
davide <davidecorsinimilano@gmail.com>
2021-01-13 15:44:17 +0100

    push changed color

--- js/script.js
+++ js/script.js
@@ -3,12 +3,16 @@ $(document).ready(function(){
     $('.with-dropdown').hover(function(){
         $(this).find('.dropdown-menu').toggleClass('active');
         $(this).toggleClass('nero');
+        $(this).find('a').toggleClass('black');
     });
     
     $('.with-dropdown-click').click(function(){
         $(this).find('.dropdown-menu.lenguage').toggleClass('active');
-        $(this).find('i#arrow-down').toggleClass("fa-arrow-down fa-arrow-up")
+        $(this).find('i#arrow-down').toggleClass("fa-arrow-down fa-arrow-up");
+        $(this).find('a').toggleClass('black');
     });
+
+    
 });
 
 /* <i id="arrow-up" class="fas fa-arrow-up"></i> */
